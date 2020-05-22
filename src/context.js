153d86c1f22d3643b46da918fcf127af893b148c
Saloni.mca19.du@gmail.com <Saloni.mca19.du@gmail.com>
2020-05-22 16:33:56 +0530

    cart components

--- src/context.js
+++ src/context.js
@@ -12,7 +12,10 @@ class ProductProvider extends Component{
 		detailProduct: detailProduct,
 		cart:[],
 		modalOpen:false,
-		modalProduct:detailProduct
+		modalProduct:detailProduct,
+		cartSubTotal:0,
+		cartTax:0,
+		cartTotal:0
 };
 	componentDidMount(){
 		this.setProducts();
@@ -51,7 +54,7 @@ class ProductProvider extends Component{
 			()=>{
 				return{ products: tempProducts, cart:[...this.state.cart, product]};
 			},
-			()=>{ console.log(this.state);}
+			()=>{ this.addTotals();}
 		)
 
 	} 
@@ -65,7 +68,54 @@ class ProductProvider extends Component{
 		this.setState(()=>{
 		return {modalOpen:false}
 		})
+	}
+
+	increment=(id)=>{
+		console.log("increment method");
+	}
+	decrement=(id)=>{
+		console.log("decrement method");
+	}
+	removeItem=(id)=>{
+		let tempProducts=[...this.state.products];
+		let tempCart=[...this.state.cart];
+		tempCart=tempCart.filter(item => item.id!==id);
+		const index= tempProducts.indexOf(this.getItem(id));
+		let removedProduct= tempProducts[index];
+		removedProduct.inCart=false;
+		removedProduct.count=0;
+		removedProduct.total=0;
+
+		this.setState(()=>{
+			return{
+				cart:[...tempCart],
+				products:[...tempProducts]}},
+				()=>{
+					this.addTotals();
+			});
+	}
+	
 		
+	clearCart=()=>{
+		this.setState(()=>{
+			return{cart:[]};}, 
+				()=>{this.setProducts();
+
+				});
+	}
+	addTotals=()=>{
+		let subTotal=0;
+		this.state.cart.map(item=>(subTotal +=item.total))
+		const tempTax=subTotal *0.1;
+		const tax = parseFloat(tempTax.toFixed(2));
+		const total= subTotal + tax;
+		this.setState(()=>{
+			return{
+				cartSubTotal:subTotal,
+				cartTax: tax,
+				cartTotal: total
+			}
+		})
 	}
 	render(){
 		return(
@@ -74,7 +124,11 @@ class ProductProvider extends Component{
 					handleDetail:this.handleDetail,
 					addToCart:this.addToCart,
 					openModal:this.openModal,
-					closeModal:this.closeModal}}>
+					closeModal:this.closeModal,
+					increment:this.increment,
+					decrement:this.decrement,
+					removeItem:this.removeItem,
+					clearCart:this.clearCart}}>
 				{this.props.children}
 			</ProductContext.Provider>
 		)
